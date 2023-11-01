606ac13e04c37bd0098bbbd548950a08eb6c5caf
Astrid <ast.gonzalez@duocuc.cl>
2023-11-01 14:33:48 -0300

    actualizacion

--- main.py
+++ main.py
@@ -1,135 +1,103 @@
-from typing import Union, List
-from fastapi.middleware.cors import CORSMiddleware
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-# Configura el middleware de CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:4200"],  # Reemplaza con la URL de origen de tu aplicación Angular
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+professors_db = {}
+students_db = {}
 
-
-class Seccion(BaseModel):
-    nombre: str
-    horario_inicio: str
-    horario_fin: str
-
-class Curso(BaseModel):
-    nombre: str
-    secciones: List[Seccion]
-
-class Profesor(BaseModel):
+class UserBase(BaseModel):
     nombre: str
     apellido: str
     correo: str
-    telefono: int
-    cursos: List[Curso]
+    contraseña: str
 
-profesores_data = []
+class Section(BaseModel):
+    seccion: str
+    horario_inicio: str
+    horario_fin: str
 
-# Agregar datos de 5 profesores a la lista, cada uno con 3 cursos, y cada curso con 3 secciones
-profesores_data.append(Profesor(
-    nombre="Alejandro",
-    apellido="Almeja",
-    correo="p@duoc.cl",
-    telefono="77777777",
-    cursos=[
-        Curso(
-            nombre="Estadistica",
-            secciones=[
-                Seccion(nombre="Seccion1", horario_inicio="09:00 AM", horario_fin="11:00 AM"),
-                Seccion(nombre="Seccion2", horario_inicio="11:00 AM", horario_fin="01:00 PM"),
-                Seccion(nombre="Seccion3", horario_inicio="02:00 PM", horario_fin="04:00 PM"),
-            ]
-        ),
-        Curso(
-            nombre="Algebra",
-            secciones=[
-                Seccion(nombre="Seccion1", horario_inicio="10:00 AM", horario_fin="12:00 PM"),
-                Seccion(nombre="Seccion2", horario_inicio="01:00 PM", horario_fin="03:00 PM"),
-                Seccion(nombre="Seccion3", horario_inicio="03:00 PM", horario_fin="05:00 PM"),
-            ]
-        ),
-        Curso(
-            nombre="Matematica",
-            secciones=[
-                Seccion(nombre="Seccion1", horario_inicio="09:30 AM", horario_fin="11:30 AM"),
-                Seccion(nombre="Seccion2", horario_inicio="11:30 AM", horario_fin="01:30 PM"),
-                Seccion(nombre="Seccion3", horario_inicio="02:30 PM", horario_fin="04:30 PM"),
-            ]
-        )
+class Professor(UserBase):
+    materias: List[str]
+    secciones: List[Section]
+
+class Student(UserBase):
+    materias_asistencia: List[str]
+
+
+# Ingresar datos de ejemplo
+professors_db["juan@gmail.com"] = Professor(
+    nombre="Juan",
+    apellido="González",
+    correo="juan@gmail.com",
+    contraseña="password123",
+    materias=["Matemáticas", "Física"],
+    secciones=[
+        Section(seccion="A", horario_inicio="08:00", horario_fin="10:00"),
+        Section(seccion="B", horario_inicio="10:15", horario_fin="12:15")
     ]
-))
+)
 
-profesores_data.append(Profesor(
-    nombre="Alejandra",
-    apellido="Gonzalez",
-    correo="ale@duoc.cl",
-    telefono="777456647",
-    cursos=[
-        Curso(
-            nombre="App movil",
-            secciones=[
-                Seccion(nombre="Seccion1", horario_inicio="09:00 AM", horario_fin="11:00 AM"),
-                Seccion(nombre="Seccion2", horario_inicio="11:00 AM", horario_fin="01:00 PM"),
-                Seccion(nombre="Seccion3", horario_inicio="02:00 PM", horario_fin="04:00 PM"),
-            ]
-        ),
-        Curso(
-            nombre="Web",
-            secciones=[
-                Seccion(nombre="Seccion1", horario_inicio="10:00 AM", horario_fin="12:00 PM"),
-                Seccion(nombre="Seccion2", horario_inicio="01:00 PM", horario_fin="03:00 PM"),
-                Seccion(nombre="Seccion3", horario_inicio="03:00 PM", horario_fin="05:00 PM"),
-            ]
-        ),
-        Curso(
-            nombre="Diseño",
-            secciones=[
-                Seccion(nombre="Seccion1", horario_inicio="09:30 AM", horario_fin="11:30 AM"),
-                Seccion(nombre="Seccion2", horario_inicio="11:30 AM", horario_fin="01:30 PM"),
-                Seccion(nombre="Seccion3", horario_inicio="02:30 PM", horario_fin="04:30 PM"),
-            ]
-        )
+students_db["maria@gmail.com"] = Student(
+    nombre="María",
+    apellido="Pérez",
+    correo="maria@gmail.com",
+    contraseña="password456",
+    materias_asistencia=["Matemáticas", "Física"]
+)    
+
+# Ingresar más datos de ejemplo para profesores
+professors_db["ana@gmail.com"] = Professor(
+    nombre="Ana",
+    apellido="López",
+    correo="ana@gmail.com",
+    contraseña="password789",
+    materias=["Historia", "Literatura"],
+    secciones=[
+        Section(seccion="A", horario_inicio="09:30", horario_fin="11:00"),
+        Section(seccion="B", horario_inicio="11:15", horario_fin="12:45")
     ]
-))
+)
 
-# Puedes hacer lo mismo para los otros profesores
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+students_db["carlos@gmail.com"] = Student(
+    nombre="Carlos",
+    apellido="Martínez",
+    correo="carlos@gmail.com",
+    contraseña="password101",
+    materias_asistencia=["Historia", "Matemáticas"]
+)
 
-@app.get("/profesor/{profesor_id}")
-def read_item(profesor_id: int, q: Union[str, None] = None):
-    if profesor_id < len(profesores_data):
-        return {"profesor_id": profesor_id, "profesor": profesores_data[profesor_id], "q": q}
-    else:
-        return {"message": "Profesor no encontrado"}
+students_db["laura@gmail.com"] = Student(
+    nombre="Laura",
+    apellido="Gómez",
+    correo="laura@gmail.com",
+    contraseña="password202",
+    materias_asistencia=["Literatura", "Física"]
+)
 
-@app.put("/profesor/{profesor_id}")
-def update_item(profesor_id: int, profesor: Profesor):
-    if profesor_id < len(profesores_data):
-        profesores_data[profesor_id] = profesor
-        return {"profesor_nombre": profesor.nombre, "profesor_id": profesor_id, "message": "Profesor actualizado exitosamente"}
-    else:
-        return {"message": "Profesor no encontrado"}
+
+@app.post("/professors/", response_model=Professor)
+def create_professor(professor: Professor):
+    professors_db[professor.correo] = professor
+    return professor
+
+@app.get("/professors/{correo}", response_model=Professor)
+def read_professor(correo: str):
+    if correo in professors_db:
+        return professors_db[correo]
+    raise HTTPException(status_code=404, detail="Professor not found")
+
+@app.get("/professors/{correo}/materias", response_model=List[str])
+def get_professor_materias(correo: str):
+    if correo in professors_db:
+        professor = professors_db[correo]
+        return professor.materias
+    raise HTTPException(status_code=404, detail="Professor not found")
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-@app.get("/profesor/{profesor_id}/cursos")
-def get_cursos_profesor(profesor_id: int):
-    if profesor_id < len(profesores_data):
-        cursos_profesor = profesores_data[profesor_id].cursos
-        return {"cursos": cursos_profesor}
-    else:
-        return {"message": "Profesor no encontrado"}
+
