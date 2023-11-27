cde6e00b271d0f30d688b4548fb5f4b0064b8d90
AstGon <127602477+AstGon@users.noreply.github.com>
2023-11-26 21:31:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Especifica los métodos que permites
+    allow_methods=["GET", "POST", "OPTIONS"],  # Ajusta según tus necesidades
     allow_headers=["*"],  # O especifica los encabezados permitidos
 )
 professors_db = {}
